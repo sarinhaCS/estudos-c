d63d6e03c9a6fd717886fb98f674cf37bebb1039
Sarah Serpa <sarahcruzserpa@gmail.com>
2026-05-15 23:25:19 -0300

    Update media-notas.c

--- 05-funcoes/media-notas.c
+++ 05-funcoes/media-notas.c
@@ -1,1 +1,27 @@
+//Objetivo: Calcular a média de uma nota, aplicando uma função
 #include <stdio.h>
+
+float calcularMedia(float nota1, float nota2)
+{
+    return (nota1 + nota2) / 2; //== return media
+}
+
+int main()
+{
+    float nota1, nota2, media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    media = calcularMedia(nota1, nota2);
+
+    printf("A media e: %.2f\n", media);
+
+    if (media >= 7.0) printf("Aluno aprovado!\n");
+    else printf("Aluno reprovado!\n");
+
+    return 0;
+}
