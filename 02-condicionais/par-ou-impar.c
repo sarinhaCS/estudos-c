36d3363ae66330d22e7a439ff97798dd3ecb991a
Sarah Serpa <sarahcruzserpa@gmail.com>
2026-05-15 23:03:00 -0300

    Update par-ou-impar.c

--- 02-condicionais/par-ou-impar.c
+++ 02-condicionais/par-ou-impar.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
 
 int main()
-{}
+{
+    int num;
+
+    printf("Digite um numero ");
+    scanf("%d", &num);
+
+    if(num < 0){
+        printf("O numero e negativo");
+        return 0;
+    }
+
+    if(num % 2 == 0) printf("O numero e par!");
+    else printf("O numero e impar");
+
+    return 0;
+}
